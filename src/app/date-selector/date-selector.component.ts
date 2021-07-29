28a02bc453f639b91b59dc5fbd4d428e40c875a9
Adnan Mama <adnan.mama@mckesson.com>
2021-07-29 16:46:29 -0500

    Code clean up

--- src/app/date-selector/date-selector.component.ts
+++ src/app/date-selector/date-selector.component.ts
@@ -9,7 +9,6 @@ import {default as _rollupMoment, Moment} from 'moment';
 
 const moment = _rollupMoment || _moment;
 
-
 export const MY_FORMATS = {
   parse: {
     dateInput: 'MMM - YYYY',
@@ -33,21 +32,19 @@ export const MY_FORMATS = {
 })
 export class DateSelectorComponent implements OnInit {
 
-  @ViewChild('dp') dp: MatDatepicker<Moment>;
-  date = new FormControl(moment());
+  date = new FormControl(_moment());
   minDate: Date;
   maxDate: Date;
 
   constructor() {
   }
 
-
   ngOnInit(): void {
     // Set the minimum to January 1st 20 years in the past and December 31st a year in the future.
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth();
     this.minDate = new Date(currentYear - 2, currentMonth, 1);
-    this.maxDate = new Date(currentYear, currentMonth - 1, 1);
+    this.maxDate = new Date(currentYear, currentMonth, 1);
   }
 
   chosenMonthHandler(normalizedMonth: Moment, datepicker: MatDatepicker<Moment>) {
@@ -56,7 +53,6 @@ export class DateSelectorComponent implements OnInit {
     ctrlValue.year(normalizedMonth.year());
     this.date.setValue(ctrlValue);
     datepicker.close();
-    // console.log(this.date.value);
   }
 
   streamOpened() {
